fb72344a6c6149dc7849c10cadf053beb343685a
Kononirin <kononirin@gmail.com>
2024-11-23 18:59:44 +0300

    add locators auth

---
+++ data/credentials.py
@@ -0,0 +1,7 @@
+class Credentials:
+
+    auth_login_teacher = "test_auth_login"
+    auth_password_teacher = "test_auth_pass"
+
+    auth_login_student = "test_auth_login_st"
+    auth_password_student = "test_pass_student"
