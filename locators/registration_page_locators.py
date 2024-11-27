56c7a95ae18025ce2f1c6506139be06d929c5b77
OxanaSum <oksana.sumina@gmail.com>
2024-11-27 18:40:03 +0200

    edit test_registration_student, add registration locators

---
+++ locators/registration_page_locators.py
@@ -0,0 +1,6 @@
+class RegistrationPageLocators:
+
+    button_submit_locator = "//button[@type='submit']"
+    fild_login_locator = "//input[@id='id_username']"
+    fild_password_locator = "//input[@id='id_password1']"
+    check_box_locator = "//input[@id='id_is_tutor']"
